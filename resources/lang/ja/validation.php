b46a2599dde55d2f598295b0be648d580a5e9266
shoya <m.syoya0508@gmail.com>
2022-02-24 18:36:32 +0000

    commit5

---
+++ resources/lang/ja/validation.php
@@ -0,0 +1,30 @@
+<?php
+return [
+    
+    'custom' => [
+        'attribute-name' => [
+            'rule-name' => 'custom-message',
+        ],
+        'email' => [
+            'required' => ':attributeを入力してください',
+        ],
+        'password' => [
+            'required' => ':attributeを入力してください',
+        ],
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap attribute place-holders
+    | with something more reader friendly such as E-Mail Address instead
+    | of "email". This simply helps us make messages a little cleaner.
+    |
+    */
+
+    'attributes' => [
+        'email' => 'メールアドレス',
+        'password' => 'パスワード',
+    ],
+    
+];
